ab401246ae614c4072542eb702bf0b5af6ef4ed7
Adam Lynn <adamlynn123@gmail.com>
2023-03-30 09:45:07 -0600

    supe changes

--- src/ups_project_sup.erl
+++ src/ups_project_sup.erl
@@ -32,8 +32,9 @@ init([]) ->
     ChildSpecs = [],
     {ok, {SupFlags, ChildSpecs}},
 
+    Sp_Sup = generate_spec(store_package_data_sup, supervisor, store_package_data_sup, []),
 
-    ChildSpecList = [],
+    ChildSpecList = [Sp_Sup],
     {ok, {SupFlags, ChildSpecList}}.
 
 %% internal functions
@@ -51,3 +52,24 @@ child(Module,Type)->
           shutdown => 2000,
           type => Type,
           modules => [Module]}. 
+
+generate_spec(Module,Type, Name, State)->
+%%
+%% A child Specification is a record with the following mappings.
+%%
+%% child_spec() = #{id => child_id(),       % mandatory. The name to be registered.
+%%                  start => mfargs(),      % mandatory. The module's startup function.
+%%                  restart => atom(),              % optional. Options are permanent (restart always), transient (restart only after abnormal termination), and temporary (never restart).
+%%                  shutdown => integer()|atom(),   % optional. A number or the atom infinity representing the milliseconds allowed for a soft, normal shutdown before it is killed brutally.
+%%                  type => atom(),                 % optional. Options are worker or supervisor.
+%%                  modules => [module()]|atom()}   % optional. A list of modules to be considered for upgrading
+%%                                                  % when the child's code is upgraded. The dynamic atom is used for when 
+%%                                                  % such a list is unknown, for example when the child is a 
+%%                                                  % gen_event manager with some unknown types of gen_event handler
+%%                                                  % modules to be added later.  
+        #{id => Module,
+            start => {Module,start_link,[local, Name, State]},
+            restart => permanent,
+            shutdown => 2000,
+            type => Type,
+            modules => [Module]}.
